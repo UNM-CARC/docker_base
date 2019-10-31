afa1baf266c063b176417ba9220baa68e6349c49
Patrick G. Bridges <patrickb@unm.edu>
2019-10-31 07:59:45 -0600

    More revision of build script

--- build_platforms.sh
+++ build_platforms.sh
@@ -1,7 +1,14 @@
-PLATFORMS="carc-wheeler tacc latest"
-for i in ${PLATFORMS}
+BRANCHES="carc-wheeler tacc master"
+for i in ${BRANCHES}
 do
-	git checkout $PLATFORMS \
-	&& docker build -t unmcarc/docker_base:${i} . \
-	&& docker push unmcarc/docker_base:${i}
+	if [ $i = "master" ];
+	then
+		TAG=latest
+	else
+		TAG=$i
+	fi
+	echo "Building branch ${i} as unmcarc/docker_base:${TAG}"
+	git checkout ${i} \
+	&& docker build -t unmcarc/docker_base:${TAG} . \
+	&& docker push unmcarc/docker_base:${TAG}
 done
